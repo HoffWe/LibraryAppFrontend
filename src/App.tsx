0ed95c94937f926a0d8c5f0449df4d3de09d81f4
Tomasz P <tmszpilat@gmail.com>
2023-02-12 20:49:20 +0100

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { AddReview } from './Components/AddReview';
+import "./App.css";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { AddAuthor } from "./Authors/AddAuthor";
+import { SearchAuthorByName } from "./Authors/SearchAuthorByName";
+import { ViewAuthors } from "./Authors/ViewAuthors";
+import { useAxios } from "./Authors/hook/useAxios";
+import { UpdateAuthor } from "./Authors/UpdateAutkor";
+import { AuthorHomePage } from "./Authors/AuthorHomePage";
+
 
 function App() {
- 
+
+    useAxios();
+
+    return (
+        <AuthorHomePage />
+    );
 }
 
 export default App;
