5861f0472f8bc2f84428c0fc9925cf80fa0f3f2a
HofWe <87663507+HoffWe@users.noreply.github.com>
2023-02-12 20:28:52 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import './App.css';
 import { AddReview } from './Components/AddReview';
 
 function App() {
-  return (
-    <div className="App">
-      <AddReview userName={"Testowy Test"} title={"Zmierzch"} />
-    </div>
-  );
+ 
 }
 
 export default App;
