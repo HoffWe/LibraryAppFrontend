cf016860415b3298d0c6155a054e50119664bc37
monik <Maalkolm>
2023-02-24 22:04:17 +0100

    Register - zmiana

--- src/Api/AuthApi.ts
+++ src/Api/AuthApi.ts
@@ -3,6 +3,7 @@ import { SignInRequest } from "../Models/Api/SignInRequest";
 import { SignInResponse } from "../Models/Api/SignInResponse";
 
 
+
 export class AuthApi {
   static sign(arg0: { username: import("react").Dispatch<import("react").SetStateAction<string>>; email: import("react").Dispatch<import("react").SetStateAction<string>>; password: import("react").Dispatch<import("react").SetStateAction<string>>; }) {
       throw new Error("Method not implemented.");
@@ -13,7 +14,9 @@ export class AuthApi {
     await axios.post<SignInResponse>("/auth/signin", request);
   
    
-}
 
+
+
+}
 
 
--- src/Components/Register/Register.tsx
+++ src/Components/Register/Register.tsx
@@ -1,58 +1,119 @@
-import React, {useCallback, useEffect, useState} from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import { AuthApi } from "../../Api/AuthApi";
+import {
+  InputContainer,
+  RegisterButton,
+  RegisterContainer,
+  RegisterInput,
+  StyledHeading,
+  ValidationError,
+} from "./RegisterStyle";
+import { toast } from "react-toastify";
 import { ACCESS_TOKEN } from "../../Constants/constants";
-import '../Register/Register.css';
-import LibraryPict from "../../assets/library.jpg";
+// tsconfig.json
+import UserContext from "../../Context/UserContext";
+import { useNavigate } from "react-router-dom";
+import { StyleDrawinglibrary } from "../Library/LibraryStyle";
+import LibraryPic from "../../assets/library.jpg";
 
+export const Register = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [isUsernameValid, setIsUsernameValid] = useState<boolean>(true);
+  const [isPasswordValid, setIsPasswordValid] = useState<boolean>(true);
 
-export const Register =(props: any) =>{
+  const navigate = useNavigate();
 
-    const [username, setUsername] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-    const [pass, setPass] = useState<string>('');
-    const[isEmailValid, setIsEmailValid] = useState<boolean>(true);
-    const[isPasswordValid, setIsPasswordValid] =useState <boolean>(true)
-    const [password, setPassword] = useState<string>('');
-       
-        const navigate = useNavigate(); // przekierowanie na uzytkownika
+  const { userModifier } = useContext(UserContext);
 
-        
-        const handleSubmit = (e:any) =>{
-            e.preventDefault();
-            console.log(email);
-    
-      
-            navigate('/')
+  const onLoginClicked = useCallback(async () => {
+    try {
+      const result = await AuthApi.signIn({
+        username: username,
+        password: password,
+      });
+
+      localStorage.setItem(ACCESS_TOKEN, result.data.accessToken);
+      userModifier({ ...result.data });
+      toast.success("Poprawnie zalogowano", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      navigate("/");
+    } catch (error: any) {
+      let errorMessage;
+
+      if (error.response && error.response.status === 401) {
+        errorMessage = "Podałeś błędne dane, spróbuj ponownie.";
+      } else {
+        errorMessage = "Wystąpił błąd podczas połączenia z serwerem.";
+      }
+
+      toast.error(errorMessage, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
     }
+  }, [username, password, userModifier, navigate]);
 
-    return(
+  useEffect(() => {
+    setIsUsernameValid(username.length > 0);
+  }, [username]);
 
-        <nav className="Container">
-        < div className = "Picture-Library">
-     <img src={LibraryPict} style ={{width: "100%", height: "100%"}} /> </div>  
-        
-        <div className="form-container-r">
-             
-        <form className ="register-form" onSubmit = {handleSubmit}>
-        <form autoComplete="off">
-            <label htmlFor="username">Podaj swoje imię</label>
-           <input value={username} onChange={(e)=>setUsername(e.target.value)} name="name" id="name" placeholder="Podaj swoje imię"/>
-           
-            <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e)=>setEmail(e.target.value)} type ="email" placeholder ="Podaj swój adres email" id="email" name="email"/>
-            
-            <label htmlFor="password">Hasło</label>
-            <input value={password} onChange={(e)=>setPassword(e.target.value)} type ="password" placeholder ="Podaj swoje hasło" id="password" name="password"/>
-            <button className="submit" type="submit"  disabled={!isEmailValid || !isPasswordValid}
-          onClick={Register}>Załóż konto</button>
-           
-        </form>
-        </form>
-        <a href="login" className="link" onClick={()=>props.onFormSwitch('Login')}>Jeśli posiadasz konto - Zaloguj się</a>
+  useEffect(() => {
+    setIsPasswordValid(password.length > 0);
+  }, [password]);
 
-        </div>
-        </nav>
-    )
-}
+  const onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
 
+  const onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <RegisterContainer>
+      <InputContainer>
+        <StyleDrawinglibrary style={{ backgroundImage: `url(${LibraryPic})` }}>
+          <div
+            style={{
+              width: "15vw",
+              backgroundColor: "white",
+              borderRadius: "8px",
+              padding: "32px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <StyledHeading>
+              <span style={{ color: "black" }}> REJESTRACJA</span>
+            </StyledHeading>
+            <RegisterInput
+              style={{ margin: "0 0 16px 0" }}
+              placeholder="Nazwa użytkownika"
+              type="email"
+              onChange={(e) => onUsernameChange(e)}
+            ></RegisterInput>
+            {!isUsernameValid && (
+              <ValidationError>Wpisz nazwę użytkownika</ValidationError>
+            )}
+            <RegisterInput
+              style={{ margin: "16px 0 0 0" }}
+              onChange={(e) => onPasswordChange(e)}
+              placeholder="Hasło"
+              type="password"
+            ></RegisterInput>
+            {!isPasswordValid && <ValidationError>Wpisz hasło</ValidationError>}
+            <RegisterButton
+              disabled={!isUsernameValid || !isPasswordValid}
+              onClick={onLoginClicked}
+            >
+              Zarejestruj się
+            </RegisterButton>
+          </div>
+        </StyleDrawinglibrary>
+      </InputContainer>
+    </RegisterContainer>
+  );
+};
